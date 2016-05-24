26a68464f9146bd1c648291ac2846f12ffa6b5d9
Woohyun Kim <r45r54r45@naver.com>
2016-05-24 18:43:13 +0900

    swap 함수 추가

---
+++ src/no_touch/DS_Sort_e.java
@@ -0,0 +1,9 @@
+package no_touch;
+
+public class DS_Sort_e {
+	public void swap(int[] array, int i,int j){
+		int temp=array[i];
+		array[i]=array[j];
+		array[j]=temp;
+	}
+}
--- src/sorting_algo/BubbleSort.java
+++ src/sorting_algo/BubbleSort.java
@@ -1,8 +1,9 @@
 package sorting_algo;
 
+import no_touch.DS_Sort_e;
 import no_touch.DS_Sort_i;
 
-public class BubbleSort implements DS_Sort_i{
+public class BubbleSort extends DS_Sort_e implements DS_Sort_i {
 
 	@Override
 	public int[] sort(int[] array) {
