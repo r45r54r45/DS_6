1495436cdec454008ef9f9da512ef7cf1aaabf25
jis3613 <jis3613@gmail.com>
2016-05-28 18:26:13 +0900

    IterativeMergeSort추가
    IterativeMergeSort추가

--- src/execute/Execute.java
+++ src/execute/Execute.java
@@ -10,7 +10,7 @@ public class Execute {
 		try {
 			rg=new RandomGenerater(); 
 			DS_Sort_i[] sort_algo=new DS_Sort_i[]{
-					new ShellSort1(),new ShellSort2(),new ShellSort3(),new QuickSort(), new RecursiveMergeSort(), new HeapSort()
+					new ShellSort1(),new ShellSort2(),new ShellSort3(),new QuickSort(), new RecursiveMergeSort(), new HeapSort(), new IterativeMergeSort()
 					}; //... 구현하고 추가하셈 예) {new BubbleSort(),new SelectionSort()....}
 			run(sort_algo);
 		} catch (Exception e) {
---
+++ src/sorting_algo/IterativeMergeSort.java
@@ -0,0 +1,34 @@
+package sorting_algo;
+
+import no_touch.DS_Sort_e;
+import no_touch.DS_Sort_i;
+
+public class IterativeMergeSort extends DS_Sort_e implements DS_Sort_i {
+	@Override
+	public int[] sort(int[] array){
+		MergeSort_Iterative(array);
+		return array;
+	}
+	public void merge(int[] a, int[]b, int low, int mid, int high){
+		for( int k=low; k<=high;k++)b[k]=a[k];
+		int i=low, j = mid+1;
+		for(int k=low;k<=high;k++){
+			if(i>mid) a[k]=b[j++];
+			else if(j>high) a[k]=b[i++];
+			else if(less(b[j],b[i])) a[k]=b[j++];
+			else a[k]=b[i++];
+		}
+	}
+	public void MergeSort_Iterative(int[] a){
+		int N=a.length;
+		int[] b =new int[N];
+		for(int k=1;k<N;k=k+k){
+			for(int i=0;i<N-k;i+=k+k){
+			int low=i;
+			int mid =i+k-1;
+			int high = Math.min(i+k+k-1, N-1);
+			merge(a,b,low,mid,high);
+			}
+		}
+	}	  
+}
