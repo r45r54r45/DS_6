fde2e260fcd792fb23a63ee4fa59e6971e9676cc
jis3613 <jis3613@gmail.com>
2016-05-28 15:10:38 +0900

    HeapSort추가

---
+++ src/sorting_algo/HeapSort.java
@@ -0,0 +1,57 @@
+package sorting_algo;
+
+import no_touch.DS_Sort_e;
+import no_touch.DS_Sort_i;
+
+public class HeapSort extends DS_Sort_e implements DS_Sort_i {
+	private static int[] a;
+    private static int n;
+    private static int left;
+    private static int right;
+    private static int largest;
+	public int[] sort(int[] array){
+		heapSort(array);
+		return array;
+	}
+	private void heapSort(int []a0){
+        a=a0;
+        buildheap(a);
+        
+        for(int i=n;i>0;i--){
+            exchange(0, i);
+            n=n-1;
+            maxheap(a, 0);
+        }
+    }
+	public static void buildheap(int []a){
+        n=a.length-1;
+        for(int i=n/2;i>=0;i--){
+            maxheap(a,i);
+        }
+    }
+    
+    public static void maxheap(int[] a, int i){ 
+        left=2*i;
+        right=2*i+1;
+        if(left <= n && a[left] > a[i]){
+            largest=left;
+        }
+        else{
+            largest=i;
+        }
+        
+        if(right <= n && a[right] > a[largest]){
+            largest=right;
+        }
+        if(largest!=i){
+            exchange(i,largest);
+            maxheap(a, largest);
+        }
+    }
+    
+    public static void exchange(int i, int j){
+        int t=a[i];
+        a[i]=a[j];
+        a[j]=t; 
+        }
+}
