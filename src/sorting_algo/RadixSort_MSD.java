8b8bc0aa80f9f5b9a8d06e082acac6075f8eb8c3
yunjiljjh <yunjiljjh@naver.com>
2016-05-30 11:44:08 +0900

    아싸

--- src/sorting_algo/RadixSort_MSD.java
+++ src/sorting_algo/RadixSort_MSD.java
@@ -4,30 +4,33 @@ import no_touch.DS_Sort_e;
 import no_touch.DS_Sort_i;
 
 public class RadixSort_MSD extends DS_Sort_e implements DS_Sort_i {
+	private static final int R = 256;
+	private static final int d = 4;
 	
-	private void radixSort_MSD(int[] a, int d) {
+	private void radixSort_MSD(int[] a) {
+		
+		if (a == null || a.length == 0)
+			return;
+	
+	//d=0일 때 a[i]의 맨앞자리 보려면 /1000
+	int i;
 	int N = a.length;
-	int[] aux = new int[N];
-	sort2(a,0,N-1,0,aux);
+	int []bucket = new int[R];
+	for(i=0; i < R; i++)
+		bucket[i]=0;
+	for (i=0; i<N; i++)
+		bucket[a[i]/1000]++;
+	int j =0;
+	for(i=0;0<R;i++)
+		while((bucket[i]--)>0)
+			a[j++] =i;
 	}
 	
-	private void sort2(int[] a,int low,int high,int d,int[] aux){
-		int R = 256;
-		int[] sindex = new int[R+1];
-		int[] oldindex = new int[R+1];
-		for (int i = low ; i <= high ; i++){
-			sindex[a[i]+1]++;}
-		for (int r = 0 ; r < R; r++) sindex[r+1] += sindex[r];
-		for (int r = 0 ; r < R; r++) oldindex[r] = sindex[r];
-		for (int i = low; i <= high ; i++)
-			aux[sindex[a[i]]++] = a[i];
-		for (int i = low ; i<= high ; i++) a[i] = aux[i - low];
-		for (int r = 0; r< R; r++)
-			sort2(a,low+oldindex[r],low+oldindex[r+1]-1,d+1,aux);
-		}
+
+	
 	
 	public int[] sort(int[] array) {
-		radixSort_MSD(array, 4);
+		radixSort_MSD(array);
 		return array;
 	}
 }
